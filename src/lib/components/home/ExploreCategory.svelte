adde3bc9c2328b01a67804c26c43ee1d2f1b7e6e
bono43 <bono43@nexondv.com>
2024-09-30 15:37:02 +0700

    fix: reload

--- src/lib/components/home/ExploreCategory.svelte
+++ src/lib/components/home/ExploreCategory.svelte
@@ -33,7 +33,7 @@
 			class="w-full"
 		>
 			<Carousel.Content>
-				{#each [...categoryData, ...categoryData] as item, i (item.category)}
+				{#each [...categoryData, ...categoryData] as item, i (i)}
 					<Carousel.Item class="md:basis-1/3 lg:basis-1/4">
 						<Card.Root class="border-none">
 							<Card.Content
