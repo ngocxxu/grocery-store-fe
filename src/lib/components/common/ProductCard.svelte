0a73536803dde89fea2c880d899e293903c01caf
NgocQuach <ngocquach43@gmail.com>
2024-09-30 23:50:10 +0700

    add funct star

--- src/lib/components/common/ProductCard.svelte
+++ src/lib/components/common/ProductCard.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
 	import Star from '$lib/components/common/Star.svelte';
+	import { createBoolArray } from '$lib/utils';
 </script>
 
 <Card.Root class="rounded-2xl">
@@ -9,18 +10,20 @@
 	</Card.Content>
 
 	<Card.Footer class="border-t p-5">
-		<div class="w-full flex flex-col gap-1.5">
+		<div class="flex w-full flex-col gap-1.5">
 			<div class="flex items-center justify-between">
 				<p class="text-sm font-extralight">Chocos</p>
 				<div class="flex gap-1">
-					{#each Array(5) as _, i (i)}
-						<Star filled={true} starId={3} />
+					{#each createBoolArray(4) as item, i (i)}
+						<Star filled={item} starId={3} />
 					{/each}
 				</div>
 			</div>
 			<p class="text-sm">Mixed Fruits Chocolates Pack</p>
 			<div class="flex justify-between">
-				<p class="font-semibold">$25 <span class="ml-1.5 text-sm font-light line-through">$30</span></p>
+				<p class="font-semibold">
+					$25 <span class="ml-1.5 text-sm font-light line-through">$30</span>
+				</p>
 				<p class="text-sm font-light">1 Pack</p>
 			</div>
 		</div>
