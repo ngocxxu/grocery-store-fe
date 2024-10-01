98a66bfe00de9dccbb7d4da7ba067047cfe769ea
bono43 <bono43@nexondv.com>
2024-10-01 11:19:25 +0700

    cicd 15

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 	import Header from '$lib/components/common/Header.svelte';
 	import Footer from '$lib/components/common/Footer.svelte';
+
+	export const prerender = true;
+	export const trailingSlash = 'always';
 </script>
 
 <div class="flex h-screen flex-col">
