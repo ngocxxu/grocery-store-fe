f87b808eb0393d27485a5e992581c8b0e720f1ec
bono43 <bono43@nexondv.com>
2024-10-03 11:19:54 +0700

    feat carousel

--- src/lib/components/product/ThumbSlider.svelte
+++ src/lib/components/product/ThumbSlider.svelte
@@ -1,41 +1,30 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card/index.js';
-	import type { CarouselAPI } from '$lib/components/ui/carousel/context.js';
 	import * as Carousel from '$lib/components/ui/carousel/index.js';
+	import Autoplay from 'embla-carousel-autoplay';
 
-	let api: CarouselAPI;
-	let current = 0;
-	let count = 0;
-
-	$: if (api) {
-		count = api.scrollSnapList().length;
-		current = api.selectedScrollSnap() + 1;
-
-		api.on('select', (e) => {
-			current = api.selectedScrollSnap() + 1;
-		});
-	}
+	const plugin = Autoplay({ delay: 2000, stopOnInteraction: true });
 </script>
 
-<div>
-	<Carousel.Root bind:api class="w-full max-w-xs">
-		<Carousel.Content>
-			{#each Array(5) as _, i (i)}
-				<Carousel.Item class="md:basis-1/2 lg:basis-1/3">
-					<div class="p-1">
-						<Card.Root>
-							<Card.Content class="flex aspect-square items-center justify-center p-6">
-								<span class="text-3xl font-semibold">{i + 1}</span>
-							</Card.Content>
-						</Card.Root>
-					</div>
-				</Carousel.Item>
-			{/each}
-		</Carousel.Content>
-		<!-- <Carousel.Previous />
-		<Carousel.Next /> -->
-	</Carousel.Root>
-	<div class="py-2 text-center text-sm text-muted-foreground">
-		Slide {current} of {count}
+<Carousel.Root
+	plugins={[plugin]}
+	class="relative w-full max-w-xl"
+	on:mousenter={plugin.stop}
+	on:mouseleave={plugin.reset}
+>
+	<Carousel.Content>
+		{#each Array(4) as _, i (i)}
+			<Carousel.Item>
+				<Card.Root>
+					<Card.Content class="flex aspect-square items-center justify-center">
+						<img src={`/img/product/${i + 1}.jpg`} alt={`Product ${i + 1}`} />
+					</Card.Content>
+				</Card.Root>
+			</Carousel.Item>
+		{/each}
+	</Carousel.Content>
+	<div class="absolute -bottom-6 left-1/2">
+		<Carousel.Previous />
+		<Carousel.Next />
 	</div>
-</div>
+</Carousel.Root>
